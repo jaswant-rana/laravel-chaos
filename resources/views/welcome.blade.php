7e787a960ab29e1063d238bc4db2e748bc71d16a
jaswant-rana <jaswant@ucreate.co.in>
2019-07-01 16:56:19 +0530

    add text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome Laravel
+                    Welcome To Laravel
                 </div>
 
                 <div class="links">
